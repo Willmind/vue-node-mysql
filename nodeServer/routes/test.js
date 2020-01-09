5f045a7839f129be6454b673a607f8107c18a676
Willmind <tanzhijingege@126.com>
2020-01-09 20:48:53 +0800

    更新数据库

--- nodeServer/routes/test.js
+++ nodeServer/routes/test.js
@@ -4,13 +4,13 @@ var connection=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'123456',
-    database:'world'
+    database:'user'
 });
 
 connection.connect();
 
-connection.query('SELECT*FROM city WHERE ID=1',function (error,results,fields) {
+connection.query('SELECT*FROM user WHERE id=1',function (error,results,fields) {
     if(error) throw error;
-    console.log('the solution is:',results[0].Name)
+    console.log('the solution is:',results[0].name)
     console.log('数据库连接成功！')
 })
