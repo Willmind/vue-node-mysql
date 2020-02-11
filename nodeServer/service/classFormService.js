6d85ee752704c5c827252d1cd66fdb45146197d2
Willmind <tanzhijingege@126.com>
2020-02-11 10:37:52 +0800

    更新教室申请列表

--- nodeServer/service/classFormService.js
+++ nodeServer/service/classFormService.js
@@ -7,25 +7,46 @@ function genID(length){
 //教室使用申请列表新增
 exports.addClassFormList = (req, res, next) => {
     let addData=req.body
-    let uuid= genID(8)
     console.log(addData);
-    let sql = 'INSERT INTO classForm(rid,class,phone,teacher,student,organization,teacherPhone,week,section,classroomType,whichDay,number,classRoom,text) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-    let data = [uuid, addData.class,addData.phone,addData.teacher,addData.student,addData.organization,addData.teacherPhone,addData.week,addData.section,addData.classroomType,addData.whichDay,addData.number,addData.classRoom,addData.text,]
-    db.base(sql, data, (response) => {
-        console.log(response);
-        if (response.length == 0) {
+    //更新
+    if(addData.rid){
+        console.log('更新');
+        let sql=`UPDATE classForm SET class=?,phone=?,teacher=?,student=?,organization=?,teacherPhone=?,week=?,section=?,classroomType=?,whichDay=?,number=?,classRoom=?,text=?,applyTime=? WHERE rid=?`
+        let data=[addData.class,addData.phone,addData.teacher,addData.student,addData.organization,addData.teacherPhone,addData.week,addData.section,addData.classroomType,addData.whichDay,addData.number,addData.classRoom,addData.text,addData.applyTime,addData.rid]
+        db.base(sql,data,(response)=>{
+            console.log(response);
             res.json({
                 status: '1',
-                msg: '新增失败',
-                result: ''
+                msg: '操作成功',
+                result: response
             });
-        } else {
-            res.json({
-                status: '0',
-                msg: '新增成功',
-            });
-        }
-    })
+
+        })
+    }else {
+        //新增
+        console.log('新增');
+        let uuid= genID(8)
+        console.log(addData);
+        let sql = 'INSERT INTO classForm(rid,class,phone,teacher,student,organization,teacherPhone,week,section,classroomType,whichDay,number,classRoom,text,applyTime) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+        let data = [uuid, addData.class,addData.phone,addData.teacher,addData.student,addData.organization,addData.teacherPhone,addData.week,addData.section,addData.classroomType,addData.whichDay,addData.number,addData.classRoom,addData.text,addData.applyTime]
+        db.base(sql, data, (response) => {
+            console.log(response);
+            if (response.length == 0) {
+                res.json({
+                    status: '1',
+                    msg: '操作失败',
+                    result: ''
+                });
+            } else {
+                res.json({
+                    status: '0',
+                    msg: '操作成功',
+                });
+            }
+        })
+    }
+
+
 }
 
 
