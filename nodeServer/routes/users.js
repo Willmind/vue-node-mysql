2f2bdbf0c05b51804a84b080906b1da031d9e924
Willmind <tanzhijingege@126.com>
2020-02-16 21:05:37 +0800

    学习vuex

--- nodeServer/routes/users.js
+++ nodeServer/routes/users.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const service = require('./../service/userService')
-const classService=require('./../service/classFormService')
+const classService = require('./../service/classFormService')
 
 
 /* GET users listing. */
-// 用户登录
-router.post('/getUserData',service.getUserData)
+//用户登录
+router.post('/getUserData', service.getUserData)
+//退出登录
+router.post('/doLogout', service.doLogout)
 
 
 //宿舍内务列表
-router.post('/uploadDormInterior',service.uploadDormInterior)
-router.post('/addDormInteriorList',service.addDormInteriorList)
-router.post('/dormitoryInteriorList',service.dormitoryInteriorList)
-router.post('/delDormInteriorList',service.delDormInteriorList)
-router.post('/updateDormInteriorList',service.updateDormInteriorList)
+router.post('/uploadDormInterior', service.uploadDormInterior)
+router.post('/addDormInteriorList', service.addDormInteriorList)
+router.post('/dormitoryInteriorList', service.dormitoryInteriorList)
+router.post('/delDormInteriorList', service.delDormInteriorList)
+router.post('/updateDormInteriorList', service.updateDormInteriorList)
 
 
 //新增教室列表
-router.post('/addClassFormList',classService.addClassFormList)
-router.post('/classFormList',classService.classFormList)
-router.post('/classFormQueryOne',classService.classFormQueryOne)
-router.post('/delclassFormList',classService.delclassFormList)
-
-
-
-
-
+router.post('/addClassFormList', classService.addClassFormList)
+router.post('/classFormList', classService.classFormList)
+router.post('/classFormQueryOne', classService.classFormQueryOne)
+router.post('/delclassFormList', classService.delclassFormList)
 
 
 //接口暴露，要不无法访问此接口
--- nodeServer/service/userService.js
+++ nodeServer/service/userService.js
@@ -35,6 +35,21 @@ exports.getUserData = (req, res, next) => {
     })
 
 }
+//退出登录
+exports.doLogout=function(req,res,next){
+    console.log(req);
+    return
+    req.session.destroy(function(err){
+        if(err){
+            console.log("退出失败!");
+            return;
+        }
+        //清除登录cookie
+        res.clearCookie(user_key)
+        res.redirect("/")
+    })
+}
+
 
 
 //宿舍内务列表详情页
